9858f240660f3ce76aedb5474ad2933357e1bd94
LCVillaflores <52596850+LCVillaflores@users.noreply.github.com>
2019-10-26 08:56:49 +0800

    Added Linear Operations

--- main.py
+++ main.py
@@ -5,7 +5,15 @@ b = np.arange(15).reshape(3, 5)
 c = np.ones((2, 2, 2))
 d = np.random.random((2, 3))
 
+# Operation
+e = np.array([[2, 3], [5, 4]])
+f = np.array([[8], [13]])
+g = np.linalg.inv(e)
+h = g @ f
+
 print(a)
 print(b)
 print(c)
 print(d)
+print(e)
+print(h)
