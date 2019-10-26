291a12d3028a67a86920d95c4c19111672dc9473
LCVillaflores <52596850+LCVillaflores@users.noreply.github.com>
2019-10-26 09:33:13 +0800

    Added data set

--- matrix_operations.py
+++ matrix_operations.py
@@ -1,3 +1,19 @@
+# Data Set
+# 1. 2x + 3y = 8
+#    5x + 4y = 13
+# 2. 3x + 4y + 5z = 26
+#    3x + 2y + z = 10
+#    5x + 6y + 3z = 26
+# 3. 3x + 2y + z + 4u= 26
+#    5x + 3y + 2z + 7u = 10
+#    6x + 2y + 3z + 4u = 26
+#    5x + 2y + 4z + u = 27
+# 4. 2x + 4y + 5z + 6u + 3v = 62
+#    6x + 3y + 2z + u + 4v = 52
+#    3x + 5y + 8z + 2u + 6v = 62
+#    4x + 2y + 6z + 5u + v = 62
+#    x + 4y + 3z + 6u + 2v = 49
+
 import numpy as np
 
 A = np.array([[2, 3],
